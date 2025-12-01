1c27c43db56b077d4c041fda3c5bb4ac6e09b5fd
Jhon Galvez <jhon.galvezchambi@gmail.com>
2025-12-01 11:29:47 -0500

    add initial implementation of subscriptions service with commands, queries, and resources

---
+++ services/subscriptions-service/src/main/java/pe/edu/upc/subscriptions_service/SubscriptionsServiceApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.subscriptions_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionsServiceApplication.class, args);
+	}
+
+}
