4f5c4139c438dc93b5161971e0cf72af968a1d04
Jhon Galvez <jhon.galvezchambi@gmail.com>
2025-12-01 11:28:59 -0500

    add initial implementation of chat service with commands, queries, and resources

---
+++ services/chat-service/src/test/java/pe/edu/upc/chat_service/ChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.chat_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
