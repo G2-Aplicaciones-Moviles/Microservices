bdb16c3b28b97327ecaf0ee59a3b03f49882c3a5
Jhon Galvez <jhon.galvezchambi@gmail.com>
2025-12-02 18:52:21 -0500

    feat(gateway-service): configure application properties for Eureka client and add JWT configuration

--- platform/config-service/src/main/java/pe/edu/upc/config_service/ConfigServiceApplication.java
+++ platform/config-service/src/main/java/pe/edu/upc/config_service/ConfigServiceApplication.java
@@ -2,9 +2,10 @@ package pe.edu.upc.config_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.actuate.autoconfigure.audit.AuditEventsEndpointAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {AuditEventsEndpointAutoConfiguration.class})
 @EnableConfigServer
 public class ConfigServiceApplication {
 
