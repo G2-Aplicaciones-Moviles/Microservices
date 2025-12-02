21bf21a20374d517af36140ee975813cb410fd0f
Jhon Galvez <jhon.galvezchambi@gmail.com>
2025-12-02 18:49:43 -0500

    feat(tracking-service): configure application properties and enable Eureka client for service registration

--- services/tracking-service/src/main/java/pe/edu/upc/tracking_service/TrackingServiceApplication.java
+++ services/tracking-service/src/main/java/pe/edu/upc/tracking_service/TrackingServiceApplication.java
@@ -2,9 +2,10 @@ package pe.edu.upc.tracking_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.actuate.autoconfigure.audit.AuditEventsEndpointAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {AuditEventsEndpointAutoConfiguration.class})
 @EnableFeignClients(basePackages = "pe.edu.upc.tracking_service.tracking.application.internal.outboundservices.acl.rest")
 public class TrackingServiceApplication {
 
